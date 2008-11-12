98f5f42952cfd75a464298b60a359a08d1bcb106
ogronau@web.de <ogronau@web.de@51151c1e-9051-0410-9c24-bff43347df1c>
2008-11-12 11:41:55 +0000

    Jede Menge diakritische Ersetzungen hinzugefügt

--- trunk/indexer/char_convert.cpp
+++ trunk/indexer/char_convert.cpp
@@ -1,159 +1,26 @@
-/*
- *  char_convert.cpp
- *  Wiki2Touch/wikisrvd
- *
- *  Copyright (c) 2008 by Tom Haukap.
- *
- *  This file is part of Wiki2Touch.
- *
- *  Wiki2Touch is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Wiki2Touch is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Wiki2Touch. If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
 #include "char_convert.h"
+#include <string>
 
-#include "tc_sc.inc"
+using namespace std;
 
-/*
-	Converts characters in tradional chineses to simplified chineses. Because an index
-	tables is used the convertion is rather fast.
-*/
-void tc2sc_utf8(char* data)
+void tolower_v0(unsigned char* data)
 {
-	printf("TC2SC\n");
-	if ( !data || !*data )
+	if ( !data )
 		return;
 
 	while ( *data )
 	{
 		unsigned char c = *data;
-		if ( (c & 0xE0)==0xE0 ) // three byte encoding in utf-8 starts with 1110xxxx
-		{
-			// only these are used int traditional chinese encoding
-			if ( (c>=0xE4)&& (c<=0xE9) )
-			{
-				unsigned char d = *(data+1);
-
-				unsigned short idx = tc_secondCodePos[c-0xE4][d-0x80];
-				if ( idx!=0xffff )
-				{
-					unsigned char e = *(data+2);
-					unsigned char* second = (unsigned char*) &chars_tc[idx][1];
-
-					while( d==*second++ )
-					{
-						if ( *second<e )
-							second += 4;
-						else
-						{
-							if ( *second==e )
-							{
-								int pos = *(second+1) + *(second+2)*0x100;
-
-								unsigned char* sc = (unsigned char*) chars_sc + (pos*5);
-								*data     = *sc++;
-								*(data+1) = *sc++;
-								*(data+2) = *sc++;
-							}
-							break;
-						}
-					}
-				}
-				data += 3;
-			}
-			else
-				data += 3;
-		}
-		else
-			data++;
+		if ( c<0x80 )
+			*data = tolower(c);
+		data++;
 	}
 }
 
-const unsigned char diacriticExchangeTable[] =
-{
-	// this table contains the char code for any diacritic char
-    // 0     1     2     3     4     5     6     7     8     9     a     b     c     d     e     f
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x80 - 0x8f
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x90 - 0x9f
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0xa0 - 0xaf
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0xb0 - 0xbf
-	0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x00, 0x43, 0x45, 0x45, 0x45, 0x45, 0x49, 0x49, 0x49, 0x49, // 0xc0 - 0xcf
-	0x44, 0x4E, 0x4F, 0x4F, 0x4F, 0x4F, 0x4F, 0x00, 0x4F, 0x55, 0x55, 0x55, 0x55, 0x59, 0x00, 0x00, // 0xd0 - 0xdf
-	0x61, 0x61, 0x61, 0x61, 0x61, 0x61, 0x00, 0x63, 0x65, 0x65, 0x65, 0x65, 0x69, 0x69, 0x69, 0x69, // 0xe0 - 0xef
-	0x00, 0x6E, 0x6F, 0x6F, 0x6F, 0x6F, 0x6F, 0x00, 0x6f, 0x75, 0x75, 0x75, 0x75, 0x79, 0x00, 0x00, // 0xf0 - 0xff
-};
 
-// method for no utf-8 character strings
-void exchange_diacritic_chars(char* data)
-{
-	if ( !data || !*data )
-		return;
-
-	while ( *data )
-	{
-		unsigned char c = *data;
-		if ( c>=0x80 )
-		{
-			unsigned char ex = diacriticExchangeTable[c-0x80];
-			if ( ex )
-				*data = ex;
-		}
-		*data++;
-	}
-}
-
-// method for utf-8 encoded character strings
-void exchange_diacritic_chars_utf8(char* data)
-{
-	if ( !data || !*data )
-		return;
-
-	unsigned char* dst = (unsigned char*) data;
-	while ( *data )
-	{
-		unsigned char c = *data;
-// 		if ( (c&0xc0)==0xc0 ) // beginning of a sequence with _AT LEAST_ 2 Bytes, WRONG!!!
-		if ( (c&0xe0)==0xc0 ) // beginning of a sequence with _EXACTLY_ 2 Bytes
-		{
-			int d = ((c&0x1f)<<6) + (*(data+1)&0x3f); // Lowest 5 & Lowest 6 give one character
-			if ( d>=0x80 && d<=0xff )
-			{
-				unsigned char ex = diacriticExchangeTable[d-0x80];
-				if ( ex )
-				{
-					*dst++ = ex;
-					data += 2;
-					continue;
-				}
-			}
-
-			// nothing to to, simly copy the code
-			*dst++ = *data++;
-			*dst++ = *data++;
-		}
-		else
-			*dst++ = *data++;
-	}
-
-	*dst = 0x0;
-}
-
-void tolower_utf8(char* data)
+// Exact copy of to_lower_utf8 from version 1 of the indexer
+// THIS IS FOR BACKWARD COMPATIBILITY, DO NOT CHANGE THIS!!!
+void tolower_v1(unsigned char* data)
 {
 	if ( !data )
 		return;
@@ -173,4 +40,187 @@ void tolower_utf8(char* data)
 		}
 		data++;
 	}
+}
+
+
+string lowerPhrase(string phrase, unsigned char indexVersion)
+{
+	if (phrase.empty())
+		return "";
+
+	string dest = phrase;
+	unsigned char* destStr = (unsigned char*) &dest.at(0);
+	switch (indexVersion)
+	{
+		case 0:
+			tolower_v0(destStr); // in this case, manipulating the chars on byte level preserves the length
+			break;
+		case 1:
+			tolower_v1(destStr); // in this case, manipulating the chars on byte level preserves the length
+			break;
+		case 2:
+		default:
+			unsigned int Uni;
+			unsigned int Skip = 0;
+			unsigned int l;
+			int lbound, rbound, index;
+			char temp[10];       // enough to hold any UTF-8 representation
+			while(destStr[Skip])
+			{
+				l = ToUTF(&destStr[Skip],&Uni);
+				lbound = 0;
+				rbound = 984 - 1;
+				while (lbound<=rbound)
+				{
+					index = (lbound+rbound)>>1;
+					if (toLowerTable[index].UTF32_from < Uni)
+						lbound = index + 1;
+					else if (toLowerTable[index].UTF32_from > Uni)
+						rbound = index - 1;
+					else
+					{
+						unsigned int codepoint[] = {toLowerTable[index].UTF32_to,0};
+						UTF2UTF8(codepoint,temp);
+						dest.replace(Skip,l,temp);
+						l = strlen(temp);
+						break;
+					}
+				}
+				Skip+=l;
+			}
+			break;
+	}
+	return dest;
+}
+
+
+string exchangeDiacriticChars(string phrase, unsigned char indexVersion)
+{
+	if (phrase.empty())
+		return "";
+
+	if (indexVersion == 0)
+		return phrase;
+
+	string dest = phrase;
+	unsigned char* Str = (unsigned char*) &dest.at(0);
+	unsigned int Uni;
+	unsigned int Skip = 0;
+	unsigned int l;
+	unsigned char found = 0;
+	char temp[100];
+	int lbound, rbound, index;
+
+	// for all indexVersion from 1 on, do some replacement stuff
+	while(Str[Skip])
+	{
+		l = ToUTF(&Str[Skip],&Uni);
+		if (Uni>0x7F)
+		{
+			for (int round=1;(round<=indexVersion) && (!found) ;round++)
+			{
+				lbound = 0;
+				for (int i=0;i<round;i++) lbound += diaReps[i];
+				rbound = -1;
+				for (int i=1;i<=round;i++) rbound += diaReps[i];
+				while (lbound<=rbound)
+				{
+					index = (lbound+rbound)>>1;
+					if (diacriticsTable[index].UTF32_from < Uni)
+						lbound = index + 1;
+					else if (diacriticsTable[index].UTF32_from > Uni)
+						rbound = index - 1;
+					else
+					{
+						found = 1;
+						unsigned int* codepoint = (unsigned int*) diacriticsTable[index].UTF32_tos;
+						UTF2UTF8(codepoint,temp);
+						dest.replace(Skip,l,temp);
+						l = strlen(temp);
+						break;
+					}
+				}
+			}
+		}
+		Skip+=l;
+	}
+	return dest;
+}
+
+unsigned char ToUTF(const unsigned char* Chr, unsigned int* UTF32)
+{
+	unsigned char Length = 2;
+
+	if(Chr[0]<0x80)
+	{
+		UTF32[0]=Chr[0];
+		Length=1;
+	}
+	else if ((Chr[0]&0xE0)==0xC0)
+	{
+		UTF32[0]=((Chr[0]&0x1C)>>2);
+		UTF32[0]=(UTF32[0]<<8)|((Chr[0]&0x3)<<6)|(Chr[1]&0x3F);
+	}
+	else if ((Chr[0]&0xF0)==0xE0)
+	{
+		UTF32[0]=((Chr[0]&0xF)<<4)|((Chr[1]&0x3C)>>2);
+		UTF32[0]=(UTF32[0]<<8)|((Chr[1]&0x3)<<6)|(Chr[2]&0x3F);
+		Length=3;
+	}
+	else
+	{
+		UTF32[0]=0xFFFD;
+		if ((Chr[0]&0xF8)==0xF0)
+			Length=4;
+		else if ((Chr[0]&0xFC)==0xF8)
+			Length=5;
+		else if ((Chr[0]&0xFE)==0xFC)
+			Length=6;
+		else if ((Chr[0]&0xFF)==0xFE)
+			Length=7;
+		else if (Chr[0]==0xFF)
+			Length=8;
+		else Length=1;
+	}
+
+	return Length;
+}
+
+unsigned int UTF2UTF8(unsigned int* Uni, char* U8)
+{
+	unsigned int Length=0;
+	unsigned int i=0;
+
+	while(Uni[Length])
+	{
+		if((Uni[Length]>0)&&(Uni[Length]<=0x7F))
+		{
+			U8[i++]=  Uni[Length++];
+		}
+		else if((Uni[Length]>0x7F)&&(Uni[Length]<=0x7FF))
+		{
+			U8[i++]= (Uni[Length  ] >>6 ) | 0xC0;
+			U8[i++]= (Uni[Length++]&0x3F) | 0x80;
+		}
+		else
+		{
+			U8[i++]= (Uni[Length  ] >>12) | 0xE0;
+			U8[i++]=((Uni[Length  ] >>6 ) & 0x3F) | 0x80 ;
+			U8[i++]= (Uni[Length++]       & 0x3F) | 0x80 ;
+		}
+	}
+	U8[i]=0;
+	return Length;
+}
+
+unsigned int UTF82UTF(unsigned char* U8, unsigned int* Uni)
+{
+	unsigned int i=0;
+	unsigned int Length=0;
+	while(U8[i])
+	{
+		i+=ToUTF(&U8[i],&Uni[Length++]);
+	}
+	Uni[Length]=0;
+	return Length;
 }
