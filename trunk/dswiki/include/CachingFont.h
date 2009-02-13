16c83c903a69af00e44d8475bebd0e70066486ad
ogronau@web.de <ogronau@web.de@51151c1e-9051-0410-9c24-bff43347df1c>
2009-02-13 14:47:33 +0000

    Neuer speichersparende Font-Klasse mit Caching

---
+++ trunk/dswiki/include/CachingFont.h
@@ -0,0 +1,53 @@
+//
+// C++ Interface: CachingFont
+//
+// Description:
+//
+//
+// Author: Oliver Gronau <ogronau@web.de>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef _CACHINGFONT_H
+#define _CACHINGFONT_H
+
+#include <vector>
+
+#define UNICODEBLOCKS_STATIC 8
+#define UNICODEBLOCKS_TEMPORARY 4
+
+class UnicodeBlock
+{
+	public:
+		UnicodeBlock(char* filename, unsigned char blockNr);
+		~UnicodeBlock();
+		unsigned char* getCharacterData(unsigned char Uni);
+		unsigned char Number();
+		int MemoryUsed();
+	private:
+		int*			_index; // 2 bytes would be OK either
+		unsigned char*	_data;
+		unsigned char	_blockNr;
+		int				_size_of_data;
+};
+
+class Font
+{
+	public:
+		Font(char* filename);
+		~Font();
+		unsigned char* getCharacterData(unsigned int Uni);
+		bool initOK();
+		unsigned char Height();
+		int MemoryUsed();
+	private:
+		bool _initOK;
+		char _filename[32];
+		int* blockImpressions;
+		int* cached;
+		int _lastCached;
+		std::vector<UnicodeBlock*> UB;
+};
+
+#endif
